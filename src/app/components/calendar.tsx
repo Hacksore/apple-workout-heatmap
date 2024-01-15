b7174a918cdee3890380734dcca6902524dd3dc0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-14 18:43:20 -0600

    WIP

---
+++ src/app/components/calendar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import HeatMap from "@uiw/react-heat-map";
+
+const value = [
+  { date: "2023/01/11", count: 2 },
+  { date: "2023/04/12", count: 20 },
+  { date: "2023/12/13", count: 10 },
+];
+
+export default function Calendar() {
+  return (
+    <HeatMap
+      lightingColor="red"
+      value={value}
+      style={{
+        color: "#fff",
+        "--rhm-rect": "#2d2d2d",
+      }}
+      weekLabels={["", "Mon", "", "Wed", "", "Fri", ""]}
+      width="600"
+      startDate={new Date("2023/01/01")}
+      endDate={new Date("2023/12/31")}
+    />
+  );
+}
