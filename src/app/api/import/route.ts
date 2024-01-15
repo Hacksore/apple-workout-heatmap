96ddbe0773b74caf4e9f14bd3daa374f3f8a17a3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-14 18:56:59 -0600

    Data

--- src/app/api/import/route.ts
+++ src/app/api/import/route.ts
@@ -2,9 +2,14 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   // TODO: translate the data into a workable shape
+  // TODO: add auth to make sure valid calls only work
   // TODO: insert data into db
-  
+
+  const data = await req.json();
+  console.log(data);
+
   return NextResponse.json({
     status: "ok",
+    data,
   });
 }
