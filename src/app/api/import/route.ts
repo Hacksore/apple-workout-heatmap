071aa95507cb35a37f2f0a87b52d56a11c1b20a1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-14 19:10:57 -0600

    WIP

--- src/app/api/import/route.ts
+++ src/app/api/import/route.ts
@@ -1,15 +1,41 @@
 import { NextResponse } from "next/server";
 
+interface Metric {
+  data: {
+    source: string;
+    date: string;
+    qty: number;
+  };
+  name: string;
+  units:
+  | "min"
+  | "count"
+  | "in"
+  | "count/min"
+  | "ms"
+  | "%"
+  | "kcal"
+  | "kcal/hr·kg";
+}
+
+interface HealthDataResponse {
+  data: {
+    metrics: Metric[];
+  };
+}
+
 export async function POST(req: Request) {
   // TODO: translate the data into a workable shape
   // TODO: add auth to make sure valid calls only work
   // TODO: insert data into db
 
-  const data = await req.json();
-  console.log(JSON.stringify(data));
+  const healthPayload: HealthDataResponse = await req.json();
+  const metrics = healthPayload.data.metrics;
+
+  const firstMetric = metrics[0];
 
   return NextResponse.json({
     status: "ok",
-    data,
+    firstMetric,
   });
 }
